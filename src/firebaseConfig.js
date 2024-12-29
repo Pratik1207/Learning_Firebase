8e16e057ead779286625d729541679439822202e
Pratik Anand <pratikanand0512@gmail.com>
2024-12-30 00:21:16 +0530

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDBv5vx6N8_6J4YwPdN_kUMfcqAO9gdgHk",
+  authDomain: "learnfirebase-9898c.firebaseapp.com",
+  projectId: "learnfirebase-9898c",
+  storageBucket: "learnfirebase-9898c.firebasestorage.app",
+  messagingSenderId: "330135932117",
+  appId: "1:330135932117:web:b03fbea63360dbd5b3265c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
